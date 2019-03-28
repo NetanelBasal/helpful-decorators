b7ecea6211cf2cc5c793a269da4f2ec8c7cf870b
Shahar Kazaz <shahar.kazaz@gmail.com>
2019-03-28 18:57:27 +0200

    fix(debounce) - passing params

--- src/debounce.ts
+++ src/debounce.ts
@@ -10,13 +10,13 @@ export function debounce( milliseconds : number = 0, options = {} ) {
   return function ( target : any, propertyKey : string, descriptor : PropertyDescriptor ) {
     const map = new WeakMap();
     const originalMethod = descriptor.value;
-    descriptor.value = function() {
+    descriptor.value = function(...params) {
       let debounced = map.get(this);
       if (!debounced) {
         debounced = debounceFn(originalMethod, milliseconds, options).bind(this);
         map.set(this, debounced);
       }
-      debounced();
+      debounced(...params);
     };
     return descriptor;
   }
